4e7360f9841d45e58cd47bf775019c680b044ebb
AlexStone911 <podgorodeczkij199@gmail.com>
2022-11-08 23:18:22 +0200

    initial commit

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <resource-item v-for="res in resources"
+                       :key="res.id"
+                       :title="res.title"
+                       :link="res.link"
+                       :desc="res.description">
+        </resource-item>
+    </ul>l
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default {
+    components: {
+        ResourceItem
+    },
+    inject: ['resources']
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
